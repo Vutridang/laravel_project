32db6c8cbb883ce5d8df64b31a6308619ef4dbe9
unknown <https://github.com/Vutridang/mylaravel.git>
2021-11-24 13:10:39 +0700

    Xoa childview file

--- resources/views/childview/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="py-5" style="background-color: #F94D2F;">
-            <div class="container"><p class="m-0 text-center text-white">BẢN QUYỀN &copy; WEBSITE 2021</p></div>
-        </footer>
